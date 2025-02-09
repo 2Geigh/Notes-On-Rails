288560d574499046d085f3e6d7af83001ccbed87
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-02-09 14:36:15 -0500

    routed /add to src/frontend/add.html
    - Added absolute path resolving to find the html page

--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
     const parsedURL = url.parse(req.url, true);
 
     if (req.method === "GET") {
+
         if (parsedURL.pathname === "/") {
             res.writeHead(200, {'content-type':'text/html; charset=utf-8'});
             res.end('Homepage');
         }
+
         else if (parsedURL.pathname === "/add") {
-            res.writeHead(200, {'content-type':'text/html; charset=utf-8'});
-            res.end("Add a person")
+
+            const absolute_path = path.resolve('src/frontend/add.html');
+            console.log(`Looking for a file at: ${absolute_path}`);
+            
+            fs.readFile(absolute_path, 'utf8', (error, data) => {
+                if (error) {
+                    res.writeHead(500, {'content-type':'text/html; charset=utf8'});
+                    res.end("Server error");
+                }
+                else {
+                    res.writeHead(200, {'content-type':'text/html; charset=utf8'});
+                    res.end(data);
+                }
+            });
+
         }
         else if (parsedURL.pathname === "/delete") {
             res.writeHead(200, {'content-type':'text/html; charset=utf-8'});
