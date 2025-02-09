1956b65cf3c1630e75415ac2051e7e054060565b
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-02-09 14:07:42 -0500

    Added birds-eye URL routing
    Routing for /, /add, and /delete, while parsing the url with url.parse

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-    // set HTTP headers
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
+    const parsedURL = url.parse(req.url, true);
+
+    if (req.method === "GET") {
+        if (parsedURL.pathname === "/") {
+            res.writeHead(200, {'content-type':'text/html; charset=utf-8'});
+            res.end('Homepage');
+        }
+        else if (parsedURL.pathname === "/add") {
+            res.writeHead(200, {'content-type':'text/html; charset=utf-8'});
+            res.end("Add a person")
+        }
+        else if (parsedURL.pathname === "/delete") {
+            res.writeHead(200, {'content-type':'text/html; charset=utf-8'});
+        }
+        else {
+            res.writeHead(404, {'content-type':'text/html; charset=utf-8'});
+        }
+    }
+
+    else {
+        res.writeHead(405, {'content-type':'text/html; charset=utf-8'});
+        res.end("Method not allowed");
+    }
+
 });
 
 const PORT = 9000;
